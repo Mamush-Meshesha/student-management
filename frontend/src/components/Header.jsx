98d904ce0314d756eba31f2e554be346a0fae9cf
Mamush-Meshesha <mamgithub@gmail.com>
2025-02-09 09:17:51 +0300

    project initialization

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+
+    return (
+      <div className=" h-[70px] border-b">
+        <div className="container mx-auto h-full">
+          <div className="flex justify-between items-center h-full">
+            <h1>Student Portal</h1>
+            <div className="flex items-center gap-3">
+              <h1>icon</h1>
+              <h1>mam</h1>
+              <h1>logout</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Header
