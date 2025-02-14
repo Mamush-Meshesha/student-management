fd830477cc972fc100ffa66877f78e23880deba5
Mamush-Meshesha <mamgithub@gmail.com>
2025-02-14 10:46:08 +0300

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from "express";
 import studentRoutes from "./routes/studentRoutes.js";
 import departmentRoute from "./routes/departemntRoute.js";
-import courseRoute from "./routes/courseRoutes.js"
+import courseRoute from "./routes/courseRoutes.js";
 import authRoute from "./routes/authRoute.js";
-import cookieParser from 'cookie-parser';
-import cors from "cors"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const port = 5000;
@@ -16,19 +16,19 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://student-management-five-delta.vercel.app/",
+      "https://student-management-five-delta.vercel.app",
     ],
     credentials: true,
   })
 );
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.use("/api", studentRoutes);
-app.use("/api", departmentRoute)
+app.use("/api", departmentRoute);
 app.use("/api", authRoute);
-app.use("/api", courseRoute)
+app.use("/api", courseRoute);
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
